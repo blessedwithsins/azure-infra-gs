85259092e7397eac2c7e39515556003aeab83123
Komal Makkar <komakkar@microsoft.com>
2021-01-19 12:05:25 -0500

    Update kubeconfig.tf

--- infra/modules/providers/azure/aks/kubeconfig.tf
+++ infra/modules/providers/azure/aks/kubeconfig.tf
@@ -13,9 +13,9 @@
 //  limitations under the License.
 
 resource "local_file" "cluster_credentials" {
-count             = var.kubeconfig_to_disk ? 1 : 0
+  count             = var.kubeconfig_to_disk ? 1 : 0
   sensitive_content = azurerm_kubernetes_cluster.main.kube_config_raw
-    filename          = "${var.output_directory}/${var.kubeconfig_filename}"
+  filename          = "${var.output_directory}/${var.kubeconfig_filename}"
 
   depends_on = [azurerm_kubernetes_cluster.main]
 }
