38d9f69fcc6dbcc1852b2359652bb9577c5d20b6
Jason Sangiamo <jasonsan@microsoft.com>
2021-01-25 14:04:00 -0800

    linting and removing unused env variable

--- source/azure-test-client/config.js
+++ source/azure-test-client/config.js
@@ -1,21 +1,23 @@
-'use strict';
-require('dotenv').config({ path: __dirname + '/' + process.env.ENVIRONMENT + '.env' });
+"use strict";
+require("dotenv").config({
+  path: __dirname + "/" + process.env.ENVIRONMENT + ".env",
+});
 
 module.exports = {
-    api_host: {
-      auth: `https://login.microsoftonline.com/${process.env.TENANT_ID}`,
-      legal: `https://${process.env.OSDU_HOST}/api/legal/v1`,
-      storage: `https://${process.env.OSDU_HOST}/api/storage/v2`,
-      search: `https://${process.env.OSDU_HOST}/api/search/v2`,
-    },
-    auth_params: {
-      grant_type: 'client_credentials',
-      client_id: process.env.PRINCIPAL_ID,
-      client_secret: process.env.PRINCIPAL_SECRET,
-      resource: process.env.CLIENT_ID
-    },
-    telemetry_settings: {
-        metrics: process.env.TRACK_METRICS,
-        events: process.env.TRACK_EVENTS
-    }
-  }
+  api_host: {
+    auth: `https://login.microsoftonline.com/${process.env.TENANT_ID}`,
+    legal: `https://${process.env.OSDU_HOST}/api/legal/v1`,
+    storage: `https://${process.env.OSDU_HOST}/api/storage/v2`,
+    search: `https://${process.env.OSDU_HOST}/api/search/v2`,
+  },
+  auth_params: {
+    grant_type: "client_credentials",
+    client_id: process.env.PRINCIPAL_ID,
+    client_secret: process.env.PRINCIPAL_SECRET,
+    resource: process.env.CLIENT_ID,
+  },
+  telemetry_settings: {
+    metrics: process.env.TRACK_METRICS,
+    events: process.env.TRACK_EVENTS,
+  },
+};
