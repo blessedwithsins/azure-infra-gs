4cbe4ca39aebe897fcc7dc85ba87afe2febed417
Hema Vishnu Pola <hepola@microsoft.com>
2021-01-27 11:45:57 -0500

    Update main.tf

--- infra/templates/osdu-r3-mvp/service_resources/main.tf
+++ infra/templates/osdu-r3-mvp/service_resources/main.tf
@@ -30,7 +30,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.41.0"
+      version = "=2.44.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
