e263762da4328e27b47699a5982f3a2b58a3a77d
Vivek Ojha <vivekojha@microsoft.com>
2021-06-02 13:05:15 +0530

    azurerm provider version upgrade and keda version upgrade

--- infra/templates/osdu-r3-mvp/service_resources/main.tf
+++ infra/templates/osdu-r3-mvp/service_resources/main.tf
@@ -30,7 +30,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.41.0"
+      version = "=2.46.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
