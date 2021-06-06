88979b708f9529035393cde5534aa82fd364cd89
Harshit Aggarwal <haaggarw@microsoft.com>
2021-06-06 11:26:35 +0530

    Updating UT

--- infra/templates/osdu-r3-mvp/data_partition/tests/unit/unit_test.go
+++ infra/templates/osdu-r3-mvp/data_partition/tests/unit/unit_test.go
@@ -49,7 +49,7 @@ func TestTemplate(t *testing.T) {
 		TfOptions:                       tfOptions,
 		Workspace:                       workspace,
 		PlanAssertions:                  nil,
-		ExpectedResourceCount:           174,
+		ExpectedResourceCount:           217,
 		ExpectedResourceAttributeValues: resourceDescription,
 	}
 
