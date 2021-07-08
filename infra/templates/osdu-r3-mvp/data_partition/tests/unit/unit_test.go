9e0df25d03b6727568ee9a4627b5fd7e50169471
Harshit Aggarwal <haaggarw@microsoft.com>
2021-07-08 07:54:25 +0530

    temp

--- infra/templates/osdu-r3-mvp/data_partition/tests/unit/unit_test.go
+++ infra/templates/osdu-r3-mvp/data_partition/tests/unit/unit_test.go
@@ -1,3 +1,73 @@
+// //  Copyright © Microsoft Corporation
+// //
+// //  Licensed under the Apache License, Version 2.0 (the "License");
+// //  you may not use this file except in compliance with the License.
+// //  You may obtain a copy of the License at
+// //
+// //       http://www.apache.org/licenses/LICENSE-2.0
+// //
+// //  Unless required by applicable law or agreed to in writing, software
+// //  distributed under the License is distributed on an "AS IS" BASIS,
+// //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// //  See the License for the specific language governing permissions and
+// //  limitations under the License.
+//
+// package test
+//
+// import (
+// 	"os"
+// 	"testing"
+// 	"fmt"
+//
+// 	"github.com/gruntwork-io/terratest/modules/terraform"
+// 	"github.com/microsoft/cobalt/test-harness/infratests"
+// )
+//
+// var tfOptions = &terraform.Options{
+// 	TerraformDir: "../../",
+// 	Upgrade:      true,
+// 	Vars: map[string]interface{}{
+// 		"resource_group_location": region,
+// 		"prefix":                  prefix,
+// 	},
+// 	BackendConfig: map[string]interface{}{
+// 		"storage_account_name": os.Getenv("TF_VAR_remote_state_account"),
+// 		"container_name":       os.Getenv("TF_VAR_remote_state_container"),
+// 	},
+// }
+//
+// func TestTemplate(t *testing.T) {
+// 	expectedAppDevResourceGroup := asMap(t, `{
+// 		"location": "`+region+`"
+// 	}`)
+//
+// 	resourceDescription := infratests.ResourceDescription{
+// 		"azurerm_resource_group.main": expectedAppDevResourceGroup,
+// 	}
+//
+// // 	deploy_dp_airflow := os.Getenv("TF_VAR_deploy_dp_airflow")
+// // 	resourceCount := 0
+//
+// // 	if deploy_dp_airflow == "true" {
+// // 	    fmt.Println("Deploy Airflow in DP, true")
+// // 	    resourceCount = 225
+// // 	} else {
+// //         fmt.Println("Deploy Airflow in DP, false")
+// //         resourceCount = 139
+// // 	}
+//
+// 	testFixture := infratests.UnitTestFixture{
+// 		GoTest:                          t,
+// 		TfOptions:                       tfOptions,
+// 		Workspace:                       workspace,
+// 		PlanAssertions:                  nil,
+// 		ExpectedResourceCount:           100,
+// 		ExpectedResourceAttributeValues: resourceDescription,
+// 	}
+//
+// 	infratests.RunUnitTests(&testFixture)
+// }
+
 //  Copyright © Microsoft Corporation
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,7 +87,6 @@ package test
 import (
 	"os"
 	"testing"
-	"fmt"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/microsoft/cobalt/test-harness/infratests"
@@ -45,23 +114,12 @@ func TestTemplate(t *testing.T) {
 		"azurerm_resource_group.main": expectedAppDevResourceGroup,
 	}
 
-// 	deploy_dp_airflow := os.Getenv("TF_VAR_deploy_dp_airflow")
-// 	resourceCount := 0
-
-// 	if deploy_dp_airflow == "true" {
-// 	    fmt.Println("Deploy Airflow in DP, true")
-// 	    resourceCount = 225
-// 	} else {
-//         fmt.Println("Deploy Airflow in DP, false")
-//         resourceCount = 139
-// 	}
-
 	testFixture := infratests.UnitTestFixture{
 		GoTest:                          t,
 		TfOptions:                       tfOptions,
 		Workspace:                       workspace,
 		PlanAssertions:                  nil,
-		ExpectedResourceCount:           100,
+		ExpectedResourceCount:           139,
 		ExpectedResourceAttributeValues: resourceDescription,
 	}
 
