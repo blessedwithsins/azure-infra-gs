a05f90809c9e542e6b34264c4d6a9df0a631eb9f
vineethguna <vineethguna@microsoft.com>
2021-07-28 19:38:34 +0530

    Fixed nsg rule accordingly

--- infra/modules/providers/azure/aks_deployment_resources/osdu_main.tf
+++ infra/modules/providers/azure/aks_deployment_resources/osdu_main.tf
@@ -99,7 +99,7 @@ resource "azurerm_network_security_rule" "aks-nsg-http-allow-rule" {
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
-  source_port_range           = "80"
+  source_port_range           = "*"
   destination_port_range      = "80"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
