adc999a7f19d7b8ff9c1e710f842f55b6d86f24c
Vivek Ojha <vivekojha@microsoft.com>
2021-08-04 10:19:26 +0530

    Cloning the repo instead of downloading now

--- infra/scripts/keda_upgrade_and_host_encryption.sh
+++ infra/scripts/keda_upgrade_and_host_encryption.sh
@@ -17,7 +17,8 @@ read -p "Gitops ssh public key file" sshPublicKeyFile
 read -p "Gitops ssh key file" gitopsSshKeyFile
 read -p "Terraform workspace name" tfWorkspaceName
 read -p "Resource Group Location" resourceGroupLocation
-read -p "Repo branch name with feature flag for keda v2 present on https://community.opengroup.org/osdu/platform/deployment-and-operations/infra-azure-provisioning. This branch should be in sync with your current infra setup" repoBranch
+read -p "Git Repo link with feature flag for keda v2 present.(Script assumes that repo name is infra-azure-provisioning. You can edit it if its something else)" gitRepo
+read -p "Repo branch name with feature flag for keda v2 present. This branch should be in sync with your current infra setup" repoBranch
 
 read -p "Automated Pipeline deployment (only yes is accepted as true. Any other input will be treated as manual deployment): " automatedDeployment
 
@@ -128,10 +129,14 @@ done
 set -euo pipefail
 
 echo "terraform applying with keda version updated to 2.2.0"
-curl -kLSs https://community.opengroup.org/osdu/platform/deployment-and-operations/infra-azure-provisioning/-/archive/${repoBranch}/infra-azure-provisioning-${repoBranch}.tar -o master.tar
-tar xvf master.tar
+#curl -kLSs https://community.opengroup.org/osdu/platform/deployment-and-operations/infra-azure-provisioning/-/archive/${repoBranch}/infra-azure-provisioning-${repoBranch}.tar -o master.tar
+#tar xvf master.tar
+#cd infra-azure-provisioning-keda-upgrade-1/infra/templates/osdu-r3-mvp/service_resources
 
-cd infra-azure-provisioning-keda-upgrade-1/infra/templates/osdu-r3-mvp/service_resources
+git clone --branch $repoBranch $gitRepo
+
+# Assuming repo name is infra-azure-provisioning. If its not, edit the repo name in below command and cd into service_resources.
+cd infra-azure-provisioning/infra/templates/osdu-r3-mvp/service_resources
 echo "keda_v2_enabled = true" >> override.tfvars
 terraform init -upgrade -backend-config "storage_account_name=$TF_VAR_remote_state_account" -backend-config "container_name=$TF_VAR_remote_state_container"
 
