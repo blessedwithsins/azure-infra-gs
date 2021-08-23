69dded44c467e5d4beafdc0f959b5d80389c63d8
arysingh <arysingh@microsoft.com>
2021-08-23 21:57:05 +0530

    Root

--- source/probe-test-client/config.js
+++ source/probe-test-client/config.js
@@ -9,7 +9,16 @@ module.exports = {
     entitlement: `https://${process.env.OSDU_HOST}/api/entitlements/v2`,
     legal: `https://${process.env.OSDU_HOST}/api/legal/v1`,
     storage: `https://${process.env.OSDU_HOST}/api/storage/v2`,
-    search: `https://${process.env.OSDU_HOST}/api/search/v2`,
+    notification: `https://${process.env.OSDU_HOST}/api/notification/v1`,
+    partition: `https://${process.env.OSDU_HOST}/api/partition/v1`,
+    crs_catalog: `https://${process.env.OSDU_HOST}/api/crs/catalog`,
+    crs_conversion: `https://${process.env.OSDU_HOST}/api/crs/converter`,
+    ingestion_workflow: `https://${process.env.OSDU_HOST}/api/workflow/v1`,
+    policy: `https://${process.env.OSDU_HOST}/api/policy/v1`,
+    unit: `https://${process.env.OSDU_HOST}/api/unit/v3`,
+    search: `https://${process.env.OSDU_HOST}/api/search/v2/query`,
+    schema: `https://${process.env.OSDU_HOST}/api/schema-service/v1`,
+    register: `https://${process.env.OSDU_HOST}/api/register/v1`,
   },
   auth_params: {
     grant_type: "client_credentials",
@@ -20,5 +29,6 @@ module.exports = {
   telemetry_settings: {
     metrics: process.env.TRACK_METRICS,
     events: process.env.TRACK_EVENTS,
-  }
+  },
+  subscription_id: process.env.SUBSCRIPTION_ID,
 };
